f8c19d922b1be173d7b2937b3e42c2d92e33f5df
endjiansyah <endji1507@gmail.com>
2023-08-16 12:36:26 +0700

    setup vite react

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      fontFamily:{
+          inter:['Inter'],
+          sans:['Inter'],
+          
+      }
+      ,
+      container: {
+          padding:{
+              sm: '20px',
+              md: '20px'
+          },
+          screens:{
+              xl: '1170px',
+          },
+          center:true,
+      },
+      extend: {
+          colors: {
+              primary: '#217BF4',
+              title: '#0A093D',
+              secondary : '#656464',
+              blueli : '#171648',
+          }
+      }
+    },
+  plugins: [],
+}
