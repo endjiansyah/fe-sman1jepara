fd6d392219e41a93eb436fa46b080d3edc34510b
endjiansyah <endji1507@gmail.com>
2023-08-17 17:02:47 +0700

    (+)page AdminPengumuman (crud, loading, pagination)

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 import PageBerita from './page/PageBerita'
 import PagePengumuman from './page/PagePengumuman'
 import AdminBerita from './page/AdminBerita'
+import AdminPengumuman from './page/AdminPengumuman'
 import PageLogin from './page/PageLogin'
 
 function App() {
 
   return (
-    <AdminBerita/>
+    <AdminPengumuman/>
   )
 }
 
