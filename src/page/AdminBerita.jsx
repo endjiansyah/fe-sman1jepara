15ab4dee70e5545a5818c759520a3846596587b6
endjiansyah <endji1507@gmail.com>
2023-08-17 13:39:50 +0700

    (+)loading Admin Berita, fixing form edit, fixing pagination

--- src/page/AdminBerita.jsx
+++ src/page/AdminBerita.jsx
@@ -9,7 +9,9 @@ function AdminBerita() {
         body: '',
         image: null,
         type: 2,
+        imageBerita : null,
     });
+
 
     const[page, setPage] = useState({
         current : 1,
@@ -26,16 +28,20 @@ function AdminBerita() {
         endpoint:'content',
     })
     const [pageLoading, setPageLoading] = useState(true);
+    const [formLoading, setFormLoading] = useState(false);
 
     const fetchData = async () => {
     try {
-        console.log('loading')
+        setPageLoading(true);
+
         const response = await fetch(baseapi+'content/berita?page='+page.current);
         const jsonData = await response.json();
 
         const newCurrentPage = jsonData.page;
         const newMaxPage = jsonData.maxpage;
 
+        console.log(newCurrentPage)
+        console.log(newMaxPage)
         setPagination({
             back: newCurrentPage > 1,
             next: newCurrentPage < newMaxPage,
@@ -47,9 +53,7 @@ function AdminBerita() {
         });
 
         setData(jsonData.data);
-        setPageLoading(false);
-        console.log('anjay')
-        
+        setPageLoading(false);        
 
         } catch (error) {
         console.error('Error fetching data:', error);
@@ -61,7 +65,6 @@ function AdminBerita() {
         if (page.current < page.maxpage) {
 
             page.current = (+page.current + 1)
-            console.log(page.current)
         }
         fetchData()
     }
@@ -70,7 +73,6 @@ function AdminBerita() {
         if (page.current > 1) {
 
             page.current = (+page.current - 1)
-            console.log(page.current)
         }
         fetchData()
     }
@@ -97,6 +99,7 @@ function AdminBerita() {
             body:item.body,
             image:item.image,
             type:2,
+            imageBerita: item.image
         });
 
         setMode({
@@ -108,6 +111,7 @@ function AdminBerita() {
     };
 
     const handleSubmit = async (event) => {
+        setFormLoading(true)
         event.preventDefault();
 
         // const [fileInput, setFileInput] = useState(null);
@@ -128,15 +132,14 @@ function AdminBerita() {
                 body: formData,
             });
 
-            // console.log(response.data)
             const responseData = await response.json();
-            console.log(responseData)
         
-            if (response.ok) {
-                // console.log('Data successfully sent to the server');
-
+            if (responseData.status) {
+                page.current = 1
                 fetchData();
                 modeCreate();
+                setFormLoading(false)
+
                 // Reset the form after successful submission
             } else {
                 console.error('Failed to send data to the server');
@@ -154,6 +157,8 @@ function AdminBerita() {
                 ...prevFormBerita,
                 [name]: event.target.files[0],
             }));
+            formBerita.imageBerita = URL.createObjectURL(event.target.files[0])
+
         } else {
             setFormBerita((prevFormBerita) => ({
                 ...prevFormBerita,
@@ -173,7 +178,6 @@ function AdminBerita() {
     
             if (response.ok) {
               // Lakukan tindakan setelah penghapusan berhasil
-              console.log(response);
               fetchData();
               // Refresh atau perbarui tampilan data jika diperlukan
             } else {
@@ -187,13 +191,12 @@ function AdminBerita() {
 
       
     useEffect(() => {
-        console.log(page.current);
         fetchData();
     }, []);
 
   return (
     <section id="pengumuman">
-        <div className="container min-h-[75vh]">
+        <div className="container min-h-[75vh] mb-8">
             <div className="title" id="top">
                 <h2>Berita</h2>
                 <p>
@@ -255,13 +258,13 @@ function AdminBerita() {
                     }
 
                     {pageLoading && 
-                    <div class="text-center">
+                    <div className="text-center mt-3">
                         <div role="status">
-                            <svg aria-hidden="true" class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                             </svg>
-                            <span class="sr-only">Loading...</span>
+                            <span className="sr-only">Loading...</span>
                         </div>
                     </div>
                     }
@@ -303,11 +306,7 @@ function AdminBerita() {
                                     onChange={handleChange}
                                     id="image" name="image" type="file"
                                      hidden/>
-                                    {mode.form == 'create' ? 
-                                    <img src={formBerita.image ? URL.createObjectURL(formBerita.image) : 'logosmansara.png'} className='h-24' />
-                                    :
-                                    <img src={formBerita.image ? formBerita.image : 'logosmansara.png'} className='h-24' />
-                                    }
+                                    <img src={formBerita.imageBerita ? formBerita.imageBerita : 'logosmansara.png'} className='h-24' />                              
                                     
                                 </label>
                             </div>
@@ -315,9 +314,22 @@ function AdminBerita() {
                             <hr className="my-3"/>
                             <div>
                                 <span className="flex w-full gap-3 items-center">
-                                    <button type="submit" className="flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md hover:bg-green-500 focus:outline-none focus:border-green-700 focus:ring-indigo active:bg-green-700 transition duration-150 ease-in-out">
-                                        Simpan
-                                    </button>
+                                    {!formLoading && 
+                                        <button type="submit" className="flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md hover:bg-green-500 focus:outline-none focus:border-green-700 focus:ring-indigo active:bg-green-700 transition duration-150 ease-in-out">
+                                            Simpan
+                                        </button>
+                                    }
+                                    {formLoading && 
+                                        <div className="text-center mt-3">
+                                            <div role="status">
+                                                <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                                </svg>
+                                                <span className="sr-only">Loading...</span>
+                                            </div>
+                                        </div>
+                                        }
                                     {/* @if ($message = Session::get('successktg'))
                                         <div className="text-green-600" role="alert">{{ $message }}</div>
                                     @endif */}
